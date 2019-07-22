35f68359f5a95d7d347f1f207cad3ade5e99bf26
BrenVargas <brendiux.49@gmail.com>
2019-07-22 18:37:31 -0500

    subiendo primera version modal

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-
 let map;
 
 function createMap() {
-        let options = {
-            center: { lat: 23.634501, lng: -102.552788},
-            zoom: 10
-        };
+  let options = {
+    center: { lat: 23.634501, lng: -102.552788 },
+    zoom: 10
+  };
 
-        map = new google.maps.Map(document.getElementById("map"),options);
-    }
+  map = new google.maps.Map(document.getElementById("map"), options);
+}
 
+// let registro=document.getElementById("registro");
+// let modal= document.getElementById("miModal");
+// let flex= document.getElementById("flex");
 
-    // let registro=document.getElementById("registro");
-    // let modal= document.getElementById("miModal");
-   
+// registro.addEventListener("click", function(){
+//     modal.style.display = "block";
+// })
 
-    // registro.addEventListener("click", function(){
-    //     modal.style.display = "block";
-    // })
-
- 
-
-
-
-
+// window.addEventListener("click", function(e){
+//     if(e.target == flex){
+//       modal.style.display = "none";
+//     }
+//    });
